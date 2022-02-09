ad61f981d74747e75fc86d95e01fbcc2c999f7a3
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-02-09 23:06:12 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 083822759168
+├ PULSA : 082287750102
 ├ OVO & DANA : 083822759168
 └────
 `.trim(), footer, 'Owner', '.owner')
